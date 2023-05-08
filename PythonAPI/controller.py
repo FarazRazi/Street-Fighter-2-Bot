b1e87bd009c5448ee7c71a46b4273e200e086c55
FarazRazi <72218210+FarazRazi@users.noreply.github.com>
2023-05-09 01:00:36 +0500

    added src folder

--- PythonAPI/controller.py
+++ PythonAPI/controller.py
@@ -1,35 +1,44 @@
+import os
 import socket
 import json
 from game_state import GameState
-#from bot import fight
+# from bot import fight
 import sys
 from bot import Bot
 
 import argparse
 import pandas as pd
 
+
 def connect(port):
-    #For making a connection with the game
+    # For making a connection with the game
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(("127.0.0.1", port))
     server_socket.listen(5)
     (client_socket, _) = server_socket.accept()
-    print ("Connected to game!")
+    print("Connected to game!")
     return client_socket
 
+
 def send(client_socket, command):
-    #This function will send your updated command to Bizhawk so that game reacts according to your command.
+    # This function will send your updated command to Bizhawk so that game reacts according to your command.
     command_dict = command.object_to_dict()
     pay_load = json.dumps(command_dict).encode()
     client_socket.sendall(pay_load)
 
+
 def receive(client_socket):
-    #receive the game state and return game state
+    # receive the game state and return game state
     pay_load = client_socket.recv(4096)
     input_dict = json.loads(pay_load.decode())
     game_state = GameState(input_dict)
 
     return game_state
+
+
+columns = ["timer", "result", "round_started", "round_over", "p1_character", "p1_health", "p1_x", "p1_y", "p1_jumping", "p1_crouching", "p1_in_move", "p1_move", "p1_Up", "p1_Down", "p1_Right", "p1_Left", "p1_Select", "p1_Start", "p1_Y", "p1_B",
+           "p1_X", "p1_A", "p1_L", "p1_R", "p2_character", "p2_health", "p2_x", "p2_y", "p2_jumping", "p2_crouching", "p2_in_move", "p2_move", "p2_Up", "p2_Down", "p2_Right", "p2_Left", "p2_Select", "p2_Start", "p2_Y", "p2_B", "p2_X", "p2_A", "p2_L", "p2_R", "player"]
+
 
 def main():
     # Create an ArgumentParser object
@@ -39,55 +48,62 @@ def main():
     parser.add_argument('number', type=int, help='player number to control')
 
     # Add the -L argument with a default value of False
-    parser.add_argument('-L', '--learning', action='store_true', help='enable learning mode')
+    parser.add_argument('-L', '--learning',
+                        action='store_true', help='enable learning mode')
 
     # Add the -R argument with a default value of False
-    parser.add_argument('-R', '--random', action='store_true', help='enable random mode')
+    parser.add_argument('-R', '--random', action='store_true',
+                        help='enable random mode')
+
+    # Add argument for file name
+    parser.add_argument('-F', '--file', type=str,
+                        help='file name to save learning data')
+
+    if (parser.parse_args().file is not None):
+        file_name = parser.parse_args().file
+    else:
+        file_name = "learning"
 
     # Parse the command-line arguments
     args = parser.parse_args()
 
     print('Player : ', args.number)
 
-    if (args.number==1):
+    if (args.number == 1):
         client_socket = connect(9999)
-    elif (args.number==2):
+    elif (args.number == 2):
         client_socket = connect(10000)
-
 
     # args has -L tag then toggle learning mode
     learning = args.learning
-    
+
     # args has -R tag then toggle random mode
     # random = args.random
     random = True
 
-
     current_game_state = None
-    #print( current_game_state.is_round_over )
-    bot=Bot()
+    # print( current_game_state.is_round_over )
+    bot = Bot()
 
     if learning:
-        bot.learn("csvs/learning.csv")
-        
+        bot.learn("csvs/"+file_name+".csv")
+        # create a new csv file
+        if not os.path.isfile(bot.file_name):
+            temp = pd.DataFrame(columns=columns)
+            temp.to_csv(bot.file_name, header=True, index=False)
+
     while (current_game_state is None) or (not current_game_state.is_round_over):
 
         current_game_state = receive(client_socket)
         bot_command = bot.fight(current_game_state, args.number, random)
         send(client_socket, bot_command)
-    
-    if ( learning ):
+
+    if (learning):
         list = bot.dfs
         bot.dfs = pd.DataFrame(list)
 
-        # read first row of csv
-        df = pd.read_csv(bot.file_name, nrows=1)
-        # if first column is not 'timer' then add header = True
-        header = False
-        if ( df.columns[0] != 'timer' ):
-            header = True
+        bot.dfs.to_csv(bot.file_name, header=False, mode='a', index=False)
 
-        bot.dfs.to_csv(bot.file_name, mode='a', header=header, index=False)
-    
+
 if __name__ == '__main__':
-   main()
+    main()
